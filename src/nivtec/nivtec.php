7acc45b3fb65658c6aa4cb6eb620dabb6ddbfc6c
MrHumanRebel <szekelydani5g@gmail.com>
2025-04-08 09:59:44 +0000

    fix darkmode

--- src/nivtec/nivtec.php
+++ src/nivtec/nivtec.php
@@ -1,10 +1,16 @@
 <?php
-
 require_once __DIR__ . '/../common/headSecure.php';
+
+// Fetch the dark mode preference for the logged-in user
+$DBLIB->where("users_userid", $AUTH->data['users_userid']);
+$user = $DBLIB->getOne('users', ['users_dark_mode']); // Get the dark mode setting
 
 // Prepare the data to be passed into the template
 $PAGEDATA = [
     'title' => 'Nivtec Színpad Kalkulátor',
+    'USERDATA' => [
+        'users_dark_mode' => $user['users_dark_mode'] ?? 0, // Default to 0 if not set
+    ],
 ];
 
 // Render the template with the data
