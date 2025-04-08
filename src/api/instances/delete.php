146a050b63168619cf0a3d439c150cc3fdf75058
MrHumanRebel <szekelydani5g@gmail.com>
2025-04-08 10:12:04 +0000

    fix delete project crew

--- src/api/instances/delete.php
+++ src/api/instances/delete.php
@@ -1,14 +1,33 @@
 <?php
 require_once __DIR__ . '/../apiHeadSecure.php';
 
-if (!$AUTH->serverPermissionCheck("INSTANCES:DELETE") or !isset($_POST['instances_id'])) die("404");
+if (!$AUTH->instancePermissionCheck("PROJECTS:DELETE") or !isset($_POST['projects_id'])) die("404");
 
-$DBLIB->where('instances_id', $_POST['instances_id']);
-$DBLIB->where("instances_deleted", 0);
-if($DBLIB->update('instances', ["instances_deleted" => 1], 1)) {
-    $bCMS->auditLog("SOFT-DELETE-INSTANCE", "instances", "Soft delete ". $_POST['instances_id'], $AUTH->data['users_userid'],null, $_POST['instances_id']);
-    finish(true);
-} else finish(false);
+// Get all project IDs including subprojects
+$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("projects.projects_deleted", 0);
+$DBLIB->where("(projects.projects_id = ? OR projects.projects_parent_project_id = ?)", [$_POST['projects_id'], $_POST['projects_id']]);
+$projects = $DBLIB->get("projects", null, "projects.projects_id");
+
+if (!$projects) finish(false);
+
+// Extract project IDs
+$projectIds = array_column($projects, "projects_id");
+
+// Delete crew assignments for all related projects
+$DBLIB->where("projects_id", $projectIds, "IN");
+$DBLIB->delete("crewAssignments");
+
+// Mark the projects as deleted
+$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("projects.projects_deleted", 0);
+$DBLIB->where("(projects.projects_id = ? OR projects.projects_parent_project_id = ?)", [$_POST['projects_id'], $_POST['projects_id']]);
+$project = $DBLIB->update("projects", ["projects.projects_deleted" => 1], 1);
+if (!$project) finish(false);
+
+$bCMS->auditLog("DELETE", "projects", "Deleted the project and its subprojects", $AUTH->data['users_userid'], null, $_POST['projects_id']);
+finish(true);
+
 
 /**
  *  @OA\Post(
